039ceda0fec5209228d862a203cc47005aa0cea4
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-27 21:38:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   def add_item(title, price, quantity=1)
     total_price = price*quantity
     @total += total_price
-    quantity.times do 
+    quantity.times do
       @items << title 
   end
 
